5735e133b2bf8dac5347611f0b95c1319f2dd3bd
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2024-05-03 21:13:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// your code here
+function changeHeading(){
+  var inputElement1 = document.getElementById("name").value;
+  var inputElement2 = document.getElementById("year").value;
+  var changeUrl = document.getElementById("url")
+  
+  var innerHtml = `?name=${inputElement1}&year=${inputElement2} ` 
+  
+  changeUrl.innerHTML += innerHtml ;
+  
+}
